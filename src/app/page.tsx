e4443d5de88fbadbb7b91f55557272e72e3b250d
Shazaq <Shazaq@proton.me>
2025-05-01 15:41:07 +0530

    fixing a silly mess

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+export default function Home(){
+  return(
+    <div>
+      <p className="text-rose-400">
+        Hello
+      </p>
+    </div>
+  )
+}
