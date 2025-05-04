80a2d6ecf2ef9bc77f898e8d120d641f6a19d53f
Shazaq <Shazaq@proton.me>
2025-05-04 09:34:36 +0530

    Modify(global component styles)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
 import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Input } from "@/components/ui/input"
+import { Progress } from "@/components/ui/progress"
+import { Textarea } from "@/components/ui/textarea"
+
 export default function Home(){
   return(
-    <div>
-      <p className="text-rose-400">
-        Hello
-      </p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">
+            I am a button
+          </Button>
+        </div>
+        <div>
+          <Input placeholder="hi"/>
+        </div>
+        <div>
+          <Progress value={40} />
+        </div>
+        <div>
+          <Textarea placeholder="This is text area"/>
+        </div>
+        <div>
+          <Checkbox/>
+        </div>  
+      </div>
     </div>
   )
 }
